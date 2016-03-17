baff4d04dbccf19aff43c088b95678d639ee3848
Christopher Martin <cgmartin@gmail.com>
2016-03-17 17:17:04 -0400

    fix main exports

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 module.exports = {
     server: require('./lib/lite-server.js'),
-    defaults: require('./lib/config-defaults'),
-    fallbackMiddleware: require('./lib/fallback-middleware')
+    defaults: require('./lib/config-defaults')
 };
